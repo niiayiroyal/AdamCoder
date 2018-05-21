c592d041745b6d7f0eca1b23da1a277808b5c683
Sunil-Ghorpade <sunilghorpade1@gmail.com>
2018-05-21 20:44:14 +0530

    First Commit

---
+++ animal.rb
@@ -0,0 +1,25 @@
+class Animal
+
+def initialize(w,h)
+  @width,@height = w,h
+end
+
+def dispWidth
+  return @width
+
+
+  def bark
+    puts "woof"
+  end
+end
+
+end
+
+dog = Animal.new(19,20)
+
+
+puts dog.dispWidth
+
+
+
+
---
+++ smartherd.rb
@@ -0,0 +1,25 @@
+class Smartherd
+
+
+  def mymethod
+
+    puts "sunil"
+  end
+  def mysecondmethod
+    puts"i am second"
+  end
+ myobject = Smartherd.new
+  myobject.mymethod
+  myobject.mysecondmethod
+  puts myobject.class
+  puts "i am second".class
+  puts 12.class
+  puts 12.568.class
+  puts true.class
+  puts false.class
+end
+puts "a" *5
+
+
+
+
